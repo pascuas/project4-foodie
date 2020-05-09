4415fdd2ea0a2bc91daf8501102045a96b0609cf
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-09 06:10:30 -0700

    added restaurant model

--- backend/models/restaurant.js
+++ backend/models/restaurant.js
@@ -1,1 +1,24 @@
-const mongoose = require
+const mongoose = require('../db/connection')
+const Schema = mongoose.Schema
+
+
+const RestaurantSchema = new Schema({
+    Name: String,
+    Type: String,
+    Cost: String,
+    Address: String,
+    Reviews: [
+        {
+            ref: "Review",
+            type: mongoose.Schema.Types.ObjectId
+        }
+    ],
+    Images: [
+        {
+            ref: "Image",
+            type: mongoose.Schema.Types.ObjectId
+        }
+    ]
+})
+
+const Restaurant = mongoose.model('Restaurant', RestaurantSchema)
