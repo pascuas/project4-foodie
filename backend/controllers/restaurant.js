8ad0cd78df51e7be5cbcaab983ca2d0bddd6f06c
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-09 06:59:23 -0700

    added restaurant controller

---
+++ backend/controllers/restaurant.js
@@ -0,0 +1,46 @@
+const Restaurant = require('../models/restaurant')
+
+const getAll = (req, res) => {
+    Restaurant.find().populate([{
+        path: 'review',
+        model: 'Review'
+    },  {
+        path: 'image',
+        model: 'Image'
+    }]).then(restaurants => res.json(restaurants))
+}
+
+const getById = (req, res) => {
+    Restaurant.findById(req.params.id).populate([{
+        path: 'review',
+        model: 'Review'
+    },  {
+        path: 'image',
+        model: 'Image'
+    }]).then(restaurant => res.json(restaurant))
+}
+
+const create = (req, res) => {
+    Restaurant.create(req.body).then(restaurant => res.json(restaurant))
+}
+
+const update = (req, res) => {
+    Restaurant.findByIdAndUpdate(req.params.id, req.body).then(restaurant => res.json(restaurant))
+}
+
+const remove = (req, res) => {
+    Restaurant.remove({_id: req.params.id}).then(() => {
+        res.json({
+            "status": "success",
+            "msg": "restaurant deleted"
+        })
+    })
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
