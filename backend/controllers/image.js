c2f04e18577598ae05525b7601e75861990f6e64
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-09 07:23:36 -0700

    added image controller

---
+++ backend/controllers/image.js
@@ -0,0 +1,41 @@
+const Image = require('../models/image')
+const Restaurant = require('../models/restaurant')
+
+const getAll = (req, res) => {
+    Image.find().then(images => res.json(images))
+}
+
+const getById = (req, res) => {
+    Image.findById(req.params.id).then(image => {
+        res.json(image)
+    })
+}
+
+const create = (req, res) => {
+    Image.create(req.body).then(image => Restaurant.findOne({_id: req.params.Id}).then(restaurant => {
+        restaurant.Images.push(image._id)
+        restaurant.save()
+        res.json(restaurant)
+    }))
+}
+
+const update = (req, res) => {
+    Image.updateOne({_id: req.params.imageId}, {"$set": req.body}).then(image => res.json(image))
+}
+
+const remove = (req, res) => {
+    Image.remove({_id: req.params.imageId}).then(() => {
+        res.json({
+            "status": "success",
+            "msg": "image deleted"
+        })
+    })
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
