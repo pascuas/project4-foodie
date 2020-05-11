478bf718ac5376c9541026ad4833aa32abdd74b9
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-11 09:48:20 -0700

    start on landing page

--- foodie/src/App.js
+++ foodie/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <>
+      <Switch>
+        <Route exact path="/" render = {() => < LandingPage />} />
+
+      </Switch>
     </>
   );
 }
---
+++ foodie/src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage(){
+    return (
+        <>
+        <h1>This is the landing page</h1>
+        </>
+    )
+}
+
+export default LandingPage
