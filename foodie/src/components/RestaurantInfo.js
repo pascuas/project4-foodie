77bacd9d8b7601b79168792c7b25dcd9c40ec0ee
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-11 14:47:44 -0700

    rendering images

--- foodie/src/components/RestaurantInfo.js
+++ foodie/src/components/RestaurantInfo.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
+import '../css/RestaurantInfo.css'
 
-function RestaurantInfo(){
+function RestaurantInfo(props){
+    console.log('restaurant-info', props)
+    let info = []
+    for (let i=0; i< props.restaurants.length; i++)
+    if (props.restaurants[i].Name === props.match.params.restaurant){
+        info = props.restaurants[i]
+    }
+
+    console.log('info', info.Images)
+
+    if (!info.Images) {
+        return <></>
+    }
+
+    const renderImages = info.Images.map((image, index) => {
+        return(
+            <Image src={image.image} rounded />
+        )
+    })
+
     return(
         <>
         <h1>This is restaurant info</h1>
+        <div className="imageContainer">
+            {renderImages}
+        </div>
         </>
     )
 }
