3e5c9fcc3a365b22df7cdc027b9fab3d986f9318
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-11 12:22:20 -0700

    rendered a restaurant

--- foodie/src/components/Results.js
+++ foodie/src/components/Results.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Card} from 'react-bootstrap'
 
 
 function Results(props) {
@@ -8,10 +9,27 @@ function Results(props) {
     let cityRestaurants = props.restaurants.filter(restaurant => restaurant.CityState === props.city)
     console.log('cityRestaurants', cityRestaurants)
 
-    
+    const renderRestaurants = cityRestaurants.map((restaurant, index) => {
+        return(
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={restaurant.Images[0].image} />
+                <Card.Body>
+                    <Card.Title>{restaurant.Name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{restaurant.Cost}  {restaurant.Description}</Card.Subtitle>
+                    <Card.Text>
+                        {restaurant.Address}
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        )
+    })
+
     return(
         <>
             <h1>This is the results page</h1>
+            {renderRestaurants}
         </>
     )
 }
