a80371824bb1e9168cc2cbd8d9b23745c37140f6
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-11 11:53:25 -0700

    filtered for city that was searched

--- foodie/src/components/Results.js
+++ foodie/src/components/Results.js
@@ -4,6 +4,11 @@ import React from 'react'
 function Results(props) {
     console.log('results-page', props.restaurants)
     console.log('city-searched', props.city)
+
+    let cityRestaurants = props.restaurants.filter(restaurant => restaurant.CityState === props.city)
+    console.log('cityRestaurants', cityRestaurants)
+
+    
     return(
         <>
             <h1>This is the results page</h1>
