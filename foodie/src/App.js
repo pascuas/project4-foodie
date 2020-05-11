4793237bd802d47ba05b0e1c3587386225ee1713
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-11 10:14:28 -0700

    added route to results page

--- foodie/src/App.js
+++ foodie/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import Results from './components/Results'
+import {getAllRes} from './services/api-helper'
 
 function App() {
+  const [restaurants, setRestaurants] = useState([])
+  const [isLoading, setIsloading] = useState(true)
+
+  useEffect(() => {
+    const makeAPICall = async () => {
+      const resp = await getAllRes()
+      setRestaurants(resp)
+      setIsloading(false)
+    }
+    makeAPICall()
+  }, [])
+
   return (
     <>
       <Switch>
-        <Route exact path="/" render = {() => < LandingPage />} />
+        <Route exact path="/" render = {() => <LandingPage />} />
+        <Route exact path="/results" render = {() => <Results restaurants = {restaurants}/>} />
 
       </Switch>
     </>
---
+++ foodie/src/components/Results.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Results(props) {
+    console.log('results-page', props.restaurants)
+    return(
+        <>
+            <h1>This is the results page</h1>
+        </>
+    )
+}
+
+export default Results
