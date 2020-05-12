7b7f59f63411baa3834c8521c18210778cfc7351
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-12 07:40:23 -0700

    fixed rendering issue for images

--- foodie/src/components/RestaurantInfo.js
+++ foodie/src/components/RestaurantInfo.js
@@ -1,26 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Image from 'react-bootstrap/Image'
 import '../css/RestaurantInfo.css'
 import {getResById, createImage} from '../services/api-helper'
 
 function RestaurantInfo(props){
     const [newImage, setNewImage] = useState('')
+    const [info, setInfo] = useState([])
+    const [images, setImages] = useState([])
 
     console.log('restaurant-info', props)
-    let info = []
-    for (let i=0; i< props.restaurants.length; i++)
-    if (props.restaurants[i].Name === props.match.params.restaurant){
-        info = props.restaurants[i]
-    }
+    // let info = []
+    // for (let i=0; i< props.restaurants.length; i++)
+    // if (props.restaurants[i].Name === props.match.params.restaurant){
+    //     info = props.restaurants[i]
+    // }
+    useEffect(() => {
+        const APICall = async() => {
+            for (let i =0; i < props.restaurants.length; i++)
+            if (props.restaurants[i].Name === props.match.params.restaurant){
+                setInfo(props.restaurants[i])
+                setImages(props.restaurants[i].Images)
+            }
+        }
+        APICall()
+    }, [])
     
 
     console.log('info', info)
+    console.log('images', images)
 
-    if (!info.Images) {
-        return <></>
-    }
+    // if (!info.Images) {
+    //     return <></>
+    // }
 
-    const renderImages = info.Images.map((image, index) => {
+    const renderImages = images.map((image, index) => {
         return(
             <>
             {/* <Card style={{ width: '18rem' }}>
@@ -31,13 +44,13 @@ function RestaurantInfo(props){
         )
     })
 
-    const renderReviews = info.Reviews.map((review, index) => {
-        return(
-            <>
-                <p>"{review.Review}"</p>
-            </>
-        )
-    })
+    // const renderReviews = info.Reviews.map((review, index) => {
+    //     return(
+    //         <>
+    //             <p>"{review.Review}"</p>
+    //         </>
+    //     )
+    // })
 
     const imageChange = (e) => {
         setNewImage(e.target.value)
@@ -47,20 +60,20 @@ function RestaurantInfo(props){
         e.preventDefault()
         const json = await createImage(info._id, {"image": newImage})
         setNewImage('')
-        // renderPage()
+        renderPage()
     }
 
-    // const renderPage = async() => {
-    //     const json = await getResById(info._id)
-    //     let info = json
-    // }
+    const renderPage = async() => {
+        const json = await getResById(info._id)
+        setImages(json.Images)
+    }
 
     return(
         <>
         <h1>This is restaurant info</h1>
         <div className='reviewContainer'>
             <h2>Reviews</h2>
-            {renderReviews}
+            {/* {renderReviews} */}
         </div>
         <h2>Images</h2>
         <div className="imageContainer">
@@ -69,8 +82,8 @@ function RestaurantInfo(props){
         <form onSubmit={imageSubmit}>
             <label>Image Url:</label><input type="text" value={newImage} onChange={imageChange}/>
             <button>Add</button>
-        </form>
-        </>
+        </form> 
+        </> 
     )
 }
 
