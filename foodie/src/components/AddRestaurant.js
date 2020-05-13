f8540b0a80f14b73b1d7827bd1104580dae9ecfb
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-05-13 10:49:34 -0700

    used react bootstrap for form. Added to add restaurant page

--- foodie/src/components/AddRestaurant.js
+++ foodie/src/components/AddRestaurant.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import {createRes} from '../services/api-helper'
 import {Link} from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function AddRestaurant() {
     const [name, setName] = useState('')
@@ -55,7 +57,7 @@ function AddRestaurant() {
     return (
         <>
             <h1>This is add restaurant page</h1>
-            <form onSubmit={restaurantSubmit}>
+            {/* <form onSubmit={restaurantSubmit}>
                 <label>Restaurant Name:</label><input type="text" value={name} onChange={nameChange}/><br/>
                 <label>Description:</label><input type="text" value={description} onChange={descriptionChange}/><br/>
                 <label>Type:</label><input type="text" value={type} onChange={typeChange}/><br/>
@@ -64,7 +66,46 @@ function AddRestaurant() {
                 <label>Address:</label><input type="text" value={address} onChange={addressChange}/><br/>
                 <label>Phone Number:</label><input type="text" value={phoneNum} onChange={phoneChange}/><br/>
                 <button>Add Restaurant</button>
-            </form>
+            </form> */}
+            <Form onSubmit={restaurantSubmit}>
+                <Form.Group>
+                    <Form.Label>Restaurant Name</Form.Label>
+                    <Form.Control type="text" value={name} onChange={nameChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control type="text" placeholder="Cafe, Breakfast & Brunch, Pizza, etc" maxLength="25" value={description} onChange={descriptionChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Type</Form.Label>
+                    <Form.Control type="text" placeholder="Lunch, Dessert, etc" maxLength="20" value={type} onChange={typeChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Cost</Form.Label>
+                    <Form.Control as="select" type="text" value={cost} onChange={costChange}>
+                        <option>$</option>
+                        <option>$$</option>
+                        <option>$$$</option>
+                    </Form.Control>
+                    <Form.Text className="text-muted">
+                        Determine the cost of the restaurant 
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>City & State Abbreviation</Form.Label>
+                    <Form.Control type="text" placeholder="Los Angeles, CA" maxLength="30" value={cityState} onChange={cityStateChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Address:</Form.Label>
+                    <Form.Control type="text" placeholder="224 Seir Ave" maxLength="30" value={address} onChange={addressChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Phone Number:</Form.Label>
+                    <Form.Control type="text" placeholder="(123) 456-7891" value={phoneNum} onChange={phoneChange}/>
+                </Form.Group>
+                <Button type="submit">Add Restaurant</Button>
+            </Form>
+            
         </>
     )
 }
